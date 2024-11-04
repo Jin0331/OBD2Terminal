b52151d4d6a7f6c4e9ce3125cce0f95bc53ff257
jinoo <jinoo@namuplanet.com>
2024-11-04 14:20:22 +0900

    Initial Commit

---
+++ OBD2ScannerTerminal/OBD2ScannerTerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  OBD2ScannerTerminalApp.swift
+//  OBD2ScannerTerminal
+//
+//  Created by Namuplanet on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct OBD2ScannerTerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
