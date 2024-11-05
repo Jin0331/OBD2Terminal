86b7bfc19a0f8a0ea6f715784e2d2828f3855e85
jinoo <jinoo@namuplanet.com>
2024-11-05 15:30:14 +0900

    [Add] OBD2 연결을 위한 PopupView 추가

--- OBD2ScannerTerminal/Feature/Popup/OBD2ConnectPopupView.swift
+++ OBD2ScannerTerminal/Feature/Popup/OBD2ConnectPopupView.swift
@@ -8,11 +8,75 @@
 import SwiftUI
 
 struct OBD2ConnectPopupView: View {
+    let bluetoothList : BluetoothList
+    let selectItem : (BluetoothItem) -> Void
+    let searchAction : () -> Void
+    let cancleAction : () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing:20) {
+            VStack(spacing:15) {
+                Text("제트카 OBD II 연결")
+                    .fontModifier(fontSize: 20, weight: .bold, color: ColorSystem.green5ea504.rawValue)
+                Text("연결할 장치가 켜져 있는지 확인해주세요.")
+                    .fontModifier(fontSize: 16, weight: .semibold, color: ColorSystem.black.rawValue)
+            }
+            
+            List(bluetoothList.sorted, id: \.id) { bluetoothItem in
+                HStack(alignment: .center, spacing: 10) {
+                    Text(bluetoothItem.name)
+                        .fontModifier(fontSize: 16, weight: .semibold, color: ColorSystem.gray6e7f8d.rawValue)
+                    
+                    Image(systemName: "antenna.radiowaves.left.and.right.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 22.5, height: 22.5)
+                        .foregroundStyle(Color.colorForRSSI(bluetoothItem.rssi)) // RSSI에 따라 색상이 변경됨
+                    
+                    Spacer()
+                }
+                .onTapGesture {
+                    selectItem(bluetoothItem)
+                }
+            }
+            .listStyle(.plain)
+            .frame(maxWidth: 250, maxHeight: 230)
+            .scrollContentBackground(.hidden)
+            .background()
+            .shadowModifier()
+            
+                        
+            VStack(alignment: .leading, spacing: 12) {
+                Text("1. Z-CAR 항목을 클릭하여 연결")
+                    .multilineTextAlignment(.leading)
+                    .fontModifier(fontSize: 14, weight: .semibold, color: ColorSystem.gray6e7f8d.rawValue)
+                
+                Text("2. 휴대폰 B/T 성능에 따라 연결시간 발생")
+                    .multilineTextAlignment(.leading)
+                    .fontModifier(fontSize: 14, weight: .semibold, color: ColorSystem.gray6e7f8d.rawValue)
+                
+                HStack {
+                    Text("3. 초기 비번 ") +
+                    Text("1234").foregroundColor(Color(hex: ColorSystem.green5ea504.rawValue)) + Text(" 또는 ")
+                     + Text("0000").foregroundColor(Color(hex: ColorSystem.green5ea504.rawValue)) + Text(" 입력")
+                }
+                .multilineTextAlignment(.leading)
+                .fontModifier(fontSize: 14, weight: .semibold, color: ColorSystem.gray6e7f8d.rawValue)
+            }
+            
+            HStack {
+                Text("취소")
+                    .textTobuttonModifier(fontSize: 15, width: 130, height: 40, textColor: ColorSystem.white.rawValue, bgColor: ColorSystem.gray6e7f8d.rawValue) {
+                        cancleAction()
+                    }
+                
+                Text("검색")
+                    .textTobuttonModifier(fontSize: 15, width: 130, height: 40, textColor: ColorSystem.white.rawValue, bgColor: ColorSystem.green5ea504.rawValue) {
+                        searchAction()
+                    }
+            }
+        }
+        .padding(25)
+        .background(Color(hex: ColorSystem.whitee4ebf1.rawValue).cornerRadius(10))
     }
-}
-
-#Preview {
-    OBD2ConnectPopupView()
 }
