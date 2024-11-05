eefdeab749785bf23330e9b2998cd1570ede09ac
jinoo <jinoo@namuplanet.com>
2024-11-05 15:12:58 +0900

    [Add] Bluetooth Model 정의

--- OBD2ScannerTerminal/Model/BluetoothDevice.swift
+++ OBD2ScannerTerminal/Model/BluetoothDevice.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+/// 블루투스 장치 정보
+struct BluetoothDevice : Hashable {
+    /// 블루투스 장치 이름
+    public var name:String
+    
+    /// 블루투스 장치 주소
+    public var address:String
+    
+    /// 블루투스 장치 신호세기
+    public var rssi:Int
+    
+    public var lastSeen : Date
+    
+    /// 블루투스 장치 정보 문자열
+    ///  - Return: 장치명, 주소, 신호세기
+    public var description: String {
+        return "BluetoothDevice - name: \(name), address: \(address), rssi: \(rssi)"
+    }
+}
+
+typealias BluetoothDeviceList = [BluetoothDevice]
